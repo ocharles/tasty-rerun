9f3b9349f546c2b90e7225d633cf733d3f327a58
Oliver Charles <ollie@ocharles.org.uk>
2015-09-09 12:24:55 +0100

    Update Nix expressions

--- shell.nix
+++ shell.nix
@@ -1,2 +1,19 @@
 with (import <nixpkgs> {}).pkgs;
-(haskell-ng.packages.ghc7101.callPackage ./. {}).env
+let pkg = haskellngPackages.callPackage
+            ({ mkDerivation, base, containers, mtl, optparse-applicative
+             , reducers, split, stdenv, stm, tagged, tasty, transformers
+             }:
+             mkDerivation {
+               pname = "tasty-rerun";
+               version = "1.1.4";
+               src = ./.;
+               buildDepends = [
+                 base containers mtl optparse-applicative reducers split stm tagged
+                 tasty transformers
+               ];
+               homepage = "http://github.com/ocharles/tasty-rerun";
+               description = "Run tests by filtering the test tree depending on the result of previous test runs";
+               license = stdenv.lib.licenses.bsd3;
+             }) {};
+in
+  pkg.env
