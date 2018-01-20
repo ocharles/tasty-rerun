b12d56e0ebdc0e7140ee278f3dfe68aa272bf354
Oliver Charles <ollie@ocharles.org.uk>
2018-01-20 10:47:34 +0000

    Refresh shell.nix

--- shell.nix
+++ shell.nix
@@ -1,19 +1,31 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, containers, mtl, optparse-applicative
-             , reducers, split, stdenv, stm, tagged, tasty, transformers
-             }:
-             mkDerivation {
-               pname = "tasty-rerun";
-               version = "1.1.6";
-               src = ./.;
-               buildDepends = [
-                 base containers mtl optparse-applicative reducers split stm tagged
-                 tasty transformers
-               ];
-               homepage = "http://github.com/ocharles/tasty-rerun";
-               description = "Run tests by filtering the test tree depending on the result of previous test runs";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, containers, mtl, optparse-applicative
+      , reducers, split, stdenv, stm, tagged, tasty, transformers
+      }:
+      mkDerivation {
+        pname = "tasty-rerun";
+        version = "1.1.8";
+        src = ./.;
+        libraryHaskellDepends = [
+          base containers mtl optparse-applicative reducers split stm tagged
+          tasty transformers
+        ];
+        homepage = "http://github.com/ocharles/tasty-rerun";
+        description = "Run tests by filtering the test tree depending on the result of previous test runs";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
